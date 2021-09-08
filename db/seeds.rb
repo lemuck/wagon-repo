ed8d43de7b5fa0d03ff462af894ac797e543affc
ugo lahaye <lahaye.ugo@hotmail.fr>
2021-09-08 11:30:56 +0200

    seed done again

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new(email: 'bobby@bob.com', password: 'azerty', admin: true)
+user.save!
+p user.email
+
 AdminUser.create!(email: 'bob@bob.com', password: 'azerty', password_confirmation: 'azerty') if Rails.env.development?
